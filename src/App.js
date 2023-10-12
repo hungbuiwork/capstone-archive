0a7179feadb79a66db9b3d924e4601763c74fd5d
hungbuiwork <hungbuiwork@gmail.com>
2023-10-12 10:53:33 -0700

    Pages created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import ProjectView from './pages/ProjectView';
+import {Upload} from './pages/Upload';
 import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home></Home>
+      <hr></hr>
+      <ProjectView></ProjectView>
+      <hr></hr>
+      <Upload></Upload>
     </div>
   );
 }
