186f8bc0db60a6c9ef214a45c7938651f637a7d2
hungbuiwork <hungbuiwork@gmail.com>
2023-10-17 10:41:59 -0700

    Create ProjectFull.jsx

---
+++ src/components/ProjectFull.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ProjectFull = (props) => {
+    /* This will be used to render the full project information. For now, just render all information from the project,
+    not focusing on UI
+    */
+    let projectKey = props.projectKey; 
+    /*Use the projectKey to locate the documents in firebase. */
+  return (
+    <div>ProjectFull</div>
+  )
+}
