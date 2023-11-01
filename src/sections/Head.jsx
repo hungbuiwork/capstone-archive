8db6ab7bd2922eb987cf2b884ebe161d3a7d07a5
rhavv <cesgonzalez01@gmail.com>
2023-11-01 16:06:40 -0700

    test
    space only

--- src/sections/Head.jsx
+++ src/sections/Head.jsx
@@ -8,7 +8,7 @@ export const Head = () => {
           <div className=" flex border-2">
             <h1 className="alex-font spaceLetters3 " style={{ color: '#0064a4', marginLeft: '50px', marginTop: '18px', fontSize: '80px' }}>UCI</h1>
             <div className=" flex flex-col justify-center ">
-              <h2 className=" alex-font closer-vertically" style={{ color: '#79B7FF', marginLeft: '50px', marginTop: '15px', fontSize: '35px' }}>CAPSTONE</h2>
+              <h2 className="  alex-font closer-vertically" style={{ color: '#79B7FF', marginLeft: '50px', marginTop: '15px', fontSize: '35px' }}>CAPSTONE</h2>
               <h2 className=" text-3xl alex-font closer-vertically" style={{ color: '#AED3FF', marginLeft: '50px', marginTop: '-9px' }} >ARCHIVE</h2>
             </div>
           </div>
