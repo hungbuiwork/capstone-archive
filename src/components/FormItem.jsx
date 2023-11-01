7c4c1ea22a982a34589f209cb02a12564de9fec0
hungbuiwork <hungbuiwork@gmail.com>
2023-10-31 18:40:23 -0700

    Form Upload, handoff to Andy

--- src/components/FormItem.jsx
+++ src/components/FormItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const FormItem = ({ label, tooltip, children }) => {
+export const FormItem = ({ label, tooltip, required, children }) => {
   return (
     <div className=" flex flex-col ">
       <div className=" tooltip tooltip-left flex" data-tip = {tooltip}>
-        <label className=" font-bold border ">{label}<span className=" text-red-500"> * </span></label>
+        <label className=" font-bold border ">{label}<span className=" text-red-500"> { required && "*"} </span></label>
       </div>
       {children}
     </div>
