45b90740eb406a28918008b36c06c5a966a2fcc8
hungbuiwork <hungbuiwork@gmail.com>
2023-11-02 23:39:46 -0700

    deleted dummy descriptions from projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -32,7 +32,7 @@ export const Project = (props) => {
         }
         
         <h2 className=" text-center text-3xl font-extrabold">{data?.name}</h2>
-        <p className="">{"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." + data?.description}</p>
+        <p className="">{data?.description}</p>
     </a>
   );
 };
