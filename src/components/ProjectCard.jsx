df862c39e4d2949d997b41d619f91489d7dfcde8
hungbuiwork <hungbuiwork@gmail.com>
2023-11-09 02:04:39 -0800

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -29,120 +29,108 @@ const ProjectCard = ({ project }) => {
   const awards = project.awards || "N/A";
   const imageURL = project.imageURL || "";
   const videoName = project.videoName || "N/A";
+  const logoURL = project.logoURL || "N/A";
   const videoURL = project.videoURL || "";
   const companyLogoURL = project.companyLogo || "";
-  const miscURL = project.miscURL || "";
-  const posterURL = project.posterURL || "";
-  const slidesURL = project.slidesURL || "";
+  const miscURL = project.misc || "";
+  const posterURL = project.poster || "";
+  const slidesURL = project.slide || "";
 
-  console.log(teamMembers)
+  console.log(teamMembers);
 
   return (
     <div style={cardStyles} className="project-card flex flex-col">
-      <div className=" justify-between flex border-2 border-gray-300">
-        <button className=" btn btn-primary">Back</button>
-        <h2 className=" m-auto font-extrabold text-6xl border-2 border-red-400">
+      <div className=" justify-between flex flex-col border-0 border-gray-300">
+        <h2 className=" m-auto text-center font-extrabold text-6xl leading-relaxed">
           {projectName}
         </h2>
-        <div>
-          <h1 className=" font-bold text-right">{schoolYear}</h1>
-          <h2 className=" font-bold text-right">
-            {startQuarter} - {endQuarter}
-          </h2>
-        </div>
+        <h2 className=" m-auto font-bold text-xl">
+          {startQuarter} {schoolYear.split("-")[0]} - {endQuarter}{" "}
+          {schoolYear.split("-")[1]}
+        </h2>
       </div>
 
-      <div className=" border-2 border-gray-300">
-        <div className=" border-2 border-green-400 w-64 mx-auto">
-          {imageURL && (
-            <img
-              src={imageURL}
-              alt="Project Image"
-              className=" p-8 border-black rounded-full border-2"
-            />
-          )}
-          <h1 className=" font-bold text-center text-lg">{companyName}</h1>
-        </div>
-
-        <p className=" text-xl"> { description + description + description}</p>
-      </div>
-
-      <div>
-        <div>
-          <div>
-            <h1 className="font-bold text-xl">Team Members</h1>
-            {teamMembers.map(member=>
-            {
-              return <h2>{member}</h2>
-            })}
+      <div className=" border-0 border-gray-300 mt-8">
+        <div className=" flex flex-row-reverse justify-center flex-wrap mb-4">
+          <div className="flex flex-col justify-center w-64">
+            <h1 className=" font-semibold text-center text-slate-700 text-2xl">
+              partnered with
+            </h1>
+            {logoURL && (
+              <img
+                src={logoURL}
+                alt="Project Image"
+                className=" p-2 border-2"
+              />
+            )}
+            <h1 className=" font-extrabold text-center text-4xl">{companyName}</h1>
+          </div>
+          <div className=" w-[50%] ml-8">
+            {imageURL && (
+              <img src={imageURL} alt="Project Image" className="" />
+            )}
           </div>
         </div>
+        <hr className=" border-[1px] border-slate-300 my-4"></hr>
+        <p className=" text-2xl indent-8">
+          {description + description + description}
+        </p>
+        <hr className=" border-[1px] border-slate-300 my-4"></hr>
       </div>
-      <p>Department: {department}</p>
-      <p>Start Date: {startDate ? startDate.toDateString() : "N/A"}</p>
-      <p>End Date: {endDate ? endDate.toDateString() : "N/A"}</p>
-      <p>Team Members: {teamMembers}</p>
-      <p>Faculty: {faculty}</p>
-      <p>Course: {course}</p>
-      <p>School Year: {schoolYear}</p>
-      <p>Start Quarter: {startQuarter}</p>
-      <p>End Quarter: {endQuarter}</p>
-      <p>Description: {description}</p>
-      <p>Awards: {awards}</p>
-      {videoURL !== "N/A" ? (
-        <div>
-          <p>Video Name: {videoName}</p>
-          <p>Video:</p>
-          <iframe
-            title="Embedded Video"
-            width="560"
-            height="315"
-            src={videoURL}
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
+
+      <div className=" border-2 flex justify-center">
+        {miscURL && (
+ <a href={miscURL} target="_blank" rel="noopener noreferrer"  className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent">
+              Misc
+            </a>
+
+        )}
+        {posterURL && (
+            <a href={posterURL} target="_blank" rel="noopener noreferrer"  className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent">
+              Poster
+            </a>
+        )}
+        {slidesURL && (
+        <a href={slidesURL} target="_blank" rel="noopener noreferrer"  className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent">
+              Slides
+            </a>
+
+        )}
+      </div>
+
+      <div className=" flex justify-center flex-wrap my-8">
+        <div className=" mb-8">
+          {videoURL !== "N/A" ? (
+            <div className=" mx-8">
+              <p className=" font-bold text-2xl">{videoName}</p>
+              <iframe
+                title="Embedded Video"
+                width="560"
+                height="315"
+                src={videoURL}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ) : (
+            <p>Video: N/A</p>
+          )}
         </div>
-      ) : (
-        <p>Video: N/A</p>
-      )}
-      {imageURL && (
-        <img
-          src={imageURL}
-          alt="Project Image"
-          style={{ maxWidth: "100%", height: "auto" }}
-        />
-      )}
-      {companyLogoURL && (
-        <img
-          src={companyLogoURL}
-          alt="Company Logo"
-          style={{ maxWidth: "100%", height: "auto" }}
-        />
-      )}
-      {miscURL && (
-        <div>
-          <p>Misc URL:</p>
-          <a href={miscURL} target="_blank" rel="noopener noreferrer">
-            {miscURL}
-          </a>
+
+        <div className=" border-slate-300 border-2 p-6 rounded-3xl">
+          <h2 className=" font-semibold">{department}</h2>
+          <h1 className="font-extrabold text-5xl">CS114</h1>
+          <h1 className="font-bold text-xl mt-2"> Faculty</h1>
+          {faculty.map((member) => {
+            return <h2>{member}</h2>;
+          })}
+          <h1 className="font-bold text-xl mt-2">Team Members</h1>
+          {teamMembers.map((member) => {
+            return <h2>{member}</h2>;
+          })}
+          <p className=" mt-9 text-slate-400">More Project Info Soon ...</p>
         </div>
-      )}
-      {posterURL && (
-        <div>
-          <p>Poster URL:</p>
-          <a href={posterURL} target="_blank" rel="noopener noreferrer">
-            {posterURL}
-          </a>
-        </div>
-      )}
-      {slidesURL && (
-        <div>
-          <p>Slides URL:</p>
-          <a href={slidesURL} target="_blank" rel="noopener noreferrer">
-            {slidesURL}
-          </a>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
