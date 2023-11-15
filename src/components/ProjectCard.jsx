64af20993383dd8b5e2ac360b5561398f2ab160a
hungbuiwork <hungbuiwork@gmail.com>
2023-11-15 15:27:09 -0800

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,12 @@
-import React from "react";
-
-const cardStyles = {
-  border: "1px solid #ccc",
-  borderRadius: "5px",
-  padding: "20px",
-  margin: "10px",
-  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-  backgroundColor: "white",
-};
+import React, { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+import { collection, query, where, getDocs, limit } from "firebase/firestore";
 
 const ProjectCard = ({ project }) => {
-  if (!project) {
-    return <div style={cardStyles}>No project data available</div>;
-  }
+  //Information about other projects
+  const [otherProjects, setOtherProjects] = useState([]);
 
+  //Information about this project
   const startDate = project.startDate?.toDate();
   const endDate = project.endDate?.toDate();
   const companyName = project.company || "N/A";
@@ -36,10 +29,49 @@ const ProjectCard = ({ project }) => {
   const posterURL = project.poster || "";
   const slidesURL = project.slide || "";
 
-  console.log(teamMembers);
+  //Finds projects within the same department
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const q = query(
+        collection(firestore, "projects"),
+        where("department", "==", department), //TODO(backend): build indexing to filter out the actual project
+        limit(10)
+      );
+
+      try {
+        const querySnapshot = await getDocs(q);
+        const projects = [];
+        querySnapshot.forEach((doc) => {
+          projects.push({ id: doc.id, ...doc.data() });
+        });
+        setOtherProjects(projects);
+      } catch (error) {
+        console.error("Error fetching projects: ", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+
 
   return (
-    <div style={cardStyles} className="project-card flex flex-col">
+    <div className="project-card flex flex-col">
+
+      {/*Render Other Projects*/}
+      <div className=" flex flex-col">
+        <h1 className=" font-bold">Other "{department}" Projects</h1>
+        {otherProjects.length > 0 ? (
+          otherProjects.map((data) => 
+          {
+            return <a key = {data.id} href = {`/view/${data.id}`} className=" text-blue-400 font-semibold hover:text-black">{data.name}</a>
+          })
+        ) : (
+          <p>No project data available</p>
+        )}
+      </div>
+
+      {/*Title Info*/}
       <div className=" justify-between flex flex-col border-0 border-gray-300">
         <h2 className=" m-auto text-center font-extrabold text-6xl leading-relaxed">
           {projectName}
@@ -50,6 +82,7 @@ const ProjectCard = ({ project }) => {
         </h2>
       </div>
 
+      {/*Images and description*/}
       <div className=" border-0 border-gray-300 mt-8">
         <div className=" flex flex-row-reverse justify-center flex-wrap mb-4">
           <div className="flex flex-col justify-center w-64">
@@ -63,7 +96,9 @@ const ProjectCard = ({ project }) => {
                 className=" p-2 border-2"
               />
             )}
-            <h1 className=" font-extrabold text-center text-4xl">{companyName}</h1>
+            <h1 className=" font-extrabold text-center text-4xl">
+              {companyName}
+            </h1>
           </div>
           <div className=" w-[50%] ml-8">
             {imageURL && (
@@ -78,25 +113,41 @@ const ProjectCard = ({ project }) => {
         <hr className=" border-[1px] border-slate-300 my-4"></hr>
       </div>
 
+      {/*External Buttons*/}
       <div className=" border-2 flex justify-center">
         {miscURL && (
- <a href={miscURL} target="_blank" rel="noopener noreferrer"  className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent">
-              Misc
-            </a>
-
+          <a
+            href={miscURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent"
+          >
+            Misc
+          </a>
         )}
         {posterURL && (
-            <a href={posterURL} target="_blank" rel="noopener noreferrer"  className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent">
-              Poster
-            </a>
+          <a
+            href={posterURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent"
+          >
+            Poster
+          </a>
         )}
         {slidesURL && (
-        <a href={slidesURL} target="_blank" rel="noopener noreferrer"  className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent">
-              Slides
-            </a>
-
+          <a
+            href={slidesURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" py-3 px-8 border-2 border-slate-800 rounded-md m-4 text-2xl font-bold text-slate-900 hover:text-white hover:bg-blue-600 duration-300 hover:border-transparent"
+          >
+            Slides
+          </a>
         )}
       </div>
+
+      {/*Video & Project Info */}
 
       <div className=" flex justify-center flex-wrap my-8">
         <div className=" mb-8">
@@ -121,12 +172,12 @@ const ProjectCard = ({ project }) => {
           <h2 className=" font-semibold">{department}</h2>
           <h1 className="font-extrabold text-5xl">CS114</h1>
           <h1 className="font-bold text-xl mt-2"> Faculty</h1>
-          {faculty.map((member) => {
-            return <h2>{member}</h2>;
+          {faculty.map((member, i) => {
+            return <h2 key={i}>{member}</h2>;
           })}
           <h1 className="font-bold text-xl mt-2">Team Members</h1>
-          {teamMembers.map((member) => {
-            return <h2>{member}</h2>;
+          {teamMembers.map((member, i) => {
+            return <h2 key={i}>{member}</h2>;
           })}
           <p className=" mt-9 text-slate-400">More Project Info Soon ...</p>
         </div>
