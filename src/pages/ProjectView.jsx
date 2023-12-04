272b5a070c6734e05f442f856b3221b6264b1733
kpradjinata <kpradjinata@gmail.com>
2023-12-04 12:34:41 -0800

    reverted changes

--- src/pages/ProjectView.jsx
+++ src/pages/ProjectView.jsx
@@ -1,57 +1,108 @@
 import React, { useState, useEffect } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, limit } from "firebase/firestore";
 import { firestore } from "../firebase";
-import CompanyProjects from '../components/CompanyProjects';
+import { Project } from "../components/Project";
 import Fuse from 'fuse.js';
-
 export const ProjectView = () => {
+  const [projectData, setProjectData] = useState([]);
   const [selectedSponsor, setSelectedSponsor] = useState("");
   const [selectedDepartment, setSelectedDepartment] = useState("");
   const [selectedYear, setSelectedYear] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const [sponsors, setSponsors] = useState([]);
   const [departments, setDepartments] = useState([]);
   const [years, setYears] = useState([]);
-  
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState({});
+  const [projectsLimit, setProjectsLimit] = useState(12);
   useEffect(() => {
     const fetchUniqueValues = async () => {
       const uniqueCompanies = new Map();
       const uniqueDepartments = new Map();
       const uniqueYears = new Map();
 
-      try {
-        const querySnapshot = await getDocs(collection(firestore, "projects"));
+      const querySnapshot = await getDocs(collection(firestore, "projects"));
 
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          if (data.verified) {
-            if (data.company) {
-              uniqueCompanies.set(data.company, (uniqueCompanies.get(data.company) || 0) + 1);
-            }
-            if (data.department) {
-              uniqueDepartments.set(data.department, (uniqueDepartments.get(data.department) || 0) + 1);
-            }
-            if (data.schoolYear) {
-              uniqueYears.set(data.schoolYear, (uniqueYears.get(data.schoolYear) || 0) + 1);
-            }
-          }
-        });
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
 
-        setSponsors(Array.from(uniqueCompanies.entries()).sort());
-        setDepartments(Array.from(uniqueDepartments.entries()).sort());
-        setYears(Array.from(uniqueYears.entries()).sort());
-      } catch (error) {
-        console.error("Error fetching unique values:", error);
-      }
+        // Companies (sponsors)
+        if (data.company) {
+          uniqueCompanies.set(data.company, (uniqueCompanies.get(data.company) || 0) + 1);
+        }
+
+        // Departments
+        if (data.department) {
+          uniqueDepartments.set(data.department, (uniqueDepartments.get(data.department) || 0) + 1);
+        }
+
+        // Years
+        if (data.schoolYear) {
+          uniqueYears.set(data.schoolYear, (uniqueYears.get(data.schoolYear) || 0) + 1);
+        }
+      });
+
+      const sortedCompanies = Array.from(uniqueCompanies.entries()).sort();
+      const sortedDepartments = Array.from(uniqueDepartments.entries()).sort();
+      const sortedYears = Array.from(uniqueYears.entries()).sort();
+
+      setSponsors(sortedCompanies);
+      setDepartments(sortedDepartments);
+      setYears(sortedYears);
     };
 
     fetchUniqueValues();
   }, []);
-
-  const handleFilterClick = (e) => {
-    e.preventDefault();
-    // Implement the logic for filtering the CompanyProjects accordion view based on selected filters
+  useEffect(() => {
+    const handleLoad = async () => {
+      try {
+        let queryRef = query(collection(firestore, "projects"));
+        if (selectedFilters.sponsor) {
+          // Update the filter to use "company" instead of "sponsor"
+          queryRef = query(queryRef, where("company", "==", selectedFilters.sponsor));
+        }
+        if (selectedFilters.department) {
+          queryRef = query(queryRef, where("department", "==", selectedFilters.department));
+        }
+        if (selectedFilters.year) {
+          queryRef = query(queryRef, where("schoolYear", "==", selectedFilters.year));
+        }
+        // Add a filter for the "verified" field
+        queryRef = query(queryRef, where("verified", "==", true));
+        // Limit the number of projects
+        queryRef = query(queryRef, limit(projectsLimit));
+        const documentSnapshot = await getDocs(queryRef);
+        const projects = documentSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProjectData(projects);
+      } catch (error) {
+        console.error("Error loading projects:", error);
+      }
+    };
+    handleLoad();
+  }, [selectedFilters, projectsLimit]);
+  const handleFilter = () => {
+    const fuse = new Fuse(projectData, {
+      keys: ['description', 'title'], 
+      includeScore: true,
+      threshold: 0.4, 
+    });
+    const searchResults = fuse.search(searchQuery);
+    setSearchResults(searchResults.map((result) => result.item));
   };
-
+  const handleFilterClick = () => {
+    setSelectedFilters({
+      sponsor: selectedSponsor,
+      department: selectedDepartment,
+      year: selectedYear,
+    });
+    handleFilter();
+  };
+  const handleLoadMore = () => {
+    setProjectsLimit((prevLimit) => prevLimit + 12);
+  };
   return (
     <div className="text-[#313144]">
       <form className="flex justify-center flex-wrap mt-12" action="#">
@@ -63,8 +114,10 @@ export const ProjectView = () => {
             onChange={(e) => setSelectedSponsor(e.target.value)}
           >
             <option value="">All</option>
-            {sponsors.map(([company, _]) => (
-              <option key={company} value={company}>{company}</option>
+            {sponsors.map(([company, count]) => (
+              <option key={company} value={company}>
+                {`${company} (${count})`}
+              </option>
             ))}
           </select>
         </div>
@@ -76,8 +129,10 @@ export const ProjectView = () => {
             onChange={(e) => setSelectedDepartment(e.target.value)}
           >
             <option value="">All</option>
-            {departments.map(([department, _]) => (
-              <option key={department} value={department}>{department}</option>
+            {departments.map(([department, count]) => (
+              <option key={department} value={department}>
+                {`${department} (${count})`}
+              </option>
             ))}
           </select>
         </div>
@@ -89,14 +144,25 @@ export const ProjectView = () => {
             onChange={(e) => setSelectedYear(e.target.value)}
           >
             <option value="">All</option>
-            {years.map(([year, _]) => (
-              <option key={year} value={year}>{year}</option>
+            {years.map(([year, count]) => (
+              <option key={year} value={year}>
+                {`${year} (${count})`}
+              </option>
             ))}
           </select>
         </div>
+        <div className="m-2 flex flex-col">
+          <label className="font-bold">SEARCH</label>
+          <input
+            className="rounded-md border-[1.5px] border-[#C4C4C4] p-2 text-sm w-48"
+            type="text"
+            placeholder="Search by description"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
         <div className="m-2 flex flex-col justify-end">
           <button
-            type="button"
             className="rounded-md border-[1.5px] border-blue-600 bg-blue-600 hover-bg-transparent hover-text-blue-600 duration-300 w-48 p-2 font-semibold text-white cursor-pointer"
             onClick={handleFilterClick}
           >
@@ -104,12 +170,29 @@ export const ProjectView = () => {
           </button>
         </div>
       </form>
-
-      <div className="mt-4">
-        {sponsors.map(([company]) => (
-          <CompanyProjects key={company} companyName={company} />
-        ))}
+      <div className="m-4 border-2 rounded-2xl flex flex-wrap justify-center">
+        {searchResults.length > 0 ? (
+          searchResults.map((project, i) => (
+            <Project projectID={project.id} projectData={project} key={i} />
+          ))
+        ) : projectData.length > 0 ? (
+          projectData.map((project, i) => (
+            <Project projectID={project.id} projectData={project} key={i} />
+          ))
+        ) : (
+          <p>No search results found</p>
+        )}
       </div>
+      {projectData.length >= projectsLimit && (
+        <div className="flex justify-center mt-4">
+          <button
+            className="rounded-md border-[1.5px] border-blue-600 bg-blue-600 hover-bg-transparent hover-text-blue-600 duration-300 p-2 font-semibold text-white cursor-pointer"
+            onClick={handleLoadMore}
+          >
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
