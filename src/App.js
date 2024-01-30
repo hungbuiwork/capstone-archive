ec3af1aebbc3cb9fca0da5540c1d4565aa75fc4d
ElysonEva <86315508+ElysonEva@users.noreply.github.com>
2024-01-30 12:37:26 -0800

    Update App.js
    got rid of auth component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import AuthUser from './components/AuthUser';
 import { ProjectView } from './pages/ProjectView';
 import { Upload } from './pages/Upload';
 import Home from './pages/home';
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Login></Login>
-      <AuthUser />
       <Home></Home>
       <hr></hr>
       <ProjectView></ProjectView>
