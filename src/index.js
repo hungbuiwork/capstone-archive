24dd42771abf6b7a86364b133911cda685106d4c
Elyson <elyson227@gmail.com>
2024-01-31 14:31:51 -0800

    started verification for router to disallow accessing submit and verify without going through login

--- src/index.js
+++ src/index.js
@@ -1,57 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import {
   createHashRouter,
   RouterProvider,
+  Navigate
 } from "react-router-dom";
 import { ProjectView } from './pages/ProjectView';
 import { Upload } from './pages/Upload';
-// import { Welcome } from './pages/Welcome';
-// import Home from './pages/home';
 import { ProjectFull } from './components/ProjectFull';
 import { Head } from './sections/Head';
 import { VerifyProjects } from './pages/VerifyProjects';
 import { ThankYou } from './pages/ThankYou';
 import { Login } from './pages/login';
 
+const IndexWithRouter = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <div><Head></Head><ProjectView></ProjectView></div>
-  },
-  {
-    path: "/view/:projectID",
-    element: <div><Head></Head><ProjectFull></ProjectFull></div>
-  },
-  {
-    path: "/submit",
-    element: <div><Head></Head><Upload></Upload></div>
-  },
-  {
-    path: "/verify",
-    element: <div><Head></Head><VerifyProjects></VerifyProjects></div>
-  },
-  {
-    path: "/thankYou",
-    element: <ThankYou></ThankYou>
-  },
-  {
-    path: "/login/:type",
-    element: <Login></Login>,
-  }
-]);
+  useEffect(() => {
+    // Check authentication status here
+    // Set isAuthenticated to true if the user is authenticated
+
+    // NEED WAY TO TRACK AUTHENTIFICATION OR SOMETHING IDK
+
+    setIsAuthenticated(true);
+  }, []);
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <div><Head></Head><ProjectView></ProjectView></div>
+    },
+    {
+      path: "/view/:projectID",
+      element: <div><Head></Head><ProjectFull></ProjectFull></div>
+    },
+    {
+      path: "/submit",
+      element: isAuthenticated ? <div><Head></Head><Upload></Upload></div> : <Navigate to="/" />
+
+    },
+    {
+      path: "/verify",
+      element: isAuthenticated ? <div><Head></Head><VerifyProjects></VerifyProjects></div> : <Navigate to="/" />
+
+    },
+    {
+      path: "/thankYou",
+      element: <ThankYou></ThankYou>
+    },
+    {
+      path: "/login/:type",
+      element: <Login></Login>,
+    }
+  ]);
+
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<IndexWithRouter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
