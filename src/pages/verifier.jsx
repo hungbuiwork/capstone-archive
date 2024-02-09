951335161b4a98e434c4e7b312d79f2454df53e0
Elyson <elyson227@gmail.com>
2024-02-09 04:04:26 -0800

    rework login, takes into account access level, use of context, single login button

--- src/pages/verifier.jsx
+++ src/pages/verifier.jsx
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from "react";
 import SideMenu from "../components/NavigationMenu";
+import { firestore, auth, storage } from "../firebase";
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 
+// WHAT TO ADD
+// on creation of a verifier should add a new collection of students under them 
+// user
+// user/verifier 
+// user/verifier/student/etc 
 
+
+// 1. Ability to see Accounts available to them (accountComponent); 
+//  - database for students should have: 
+//      - accountName, password; projectStatus; department; ProjectID; canEdit; role; verifierID; ID; 
+//  - database for verifier: 
+//      - accountName, password; department; role; ID; listStudents; 
+// 2. Add accounts
+// 3. Delete accounts
+// 4. View projects 
+// 5. Accept projects
+// 6. Add projects with comments
 export const Verifer = () => {
+    const handleAdd = async (e) => {
+
+    }
     {
 
 
@@ -11,10 +33,7 @@ export const Verifer = () => {
 
             <div className="main" id="outer-container">
                 <div id="page-wrap">
-                    <h1>Admin Page</h1>
-                    <SideMenu></SideMenu>
-                    <link rel="stylesheet" href="/verifier" />
-
+                    <h1>Verifier Page</h1>
                 </div>
             </div>
         )
