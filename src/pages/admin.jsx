c7c083afe483f8a963c176e8e0d67596908afd0e
Yuhang Guan <yuhangg5@uci.edu>
2024-02-14 21:27:36 -0800

    create admin-style.css. and import it

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -3,6 +3,7 @@ import SideMenu from "../components/NavigationMenu";
 import "reactjs-popup/dist/index.css"; // Import the CSS file
 import { AuthContext } from "../context/AuthContext";
 import VerifierPopup from "../components/CreateNewVerifier.jsx"
+import './admin-styles.css'; 
 
 export const Admin = () => {
 
